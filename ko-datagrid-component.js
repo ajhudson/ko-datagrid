67985b9b2718a11be54a691a019d68656a6b4eda
Andy Hudson <andyhudson1979@gmail.com>
2019-07-28 17:09:44 +0100

    basic pagination

--- ko-datagrid-component.js
+++ ko-datagrid-component.js
@@ -1,13 +1,24 @@
 ko.components.register('simple-bootstrap-data-grid', {
     viewModel: function(params) {
-
-        console.log(params);
-
         var gridData = params.hasOwnProperty("data") ? params.data : {};
         var pageSize = params.hasOwnProperty("pageSize") ? params.pageSize : data().length;
+        var currentPage = ko.observable(1);
         
-        console.log(gridData());
-        console.log("");
+        var maxPages = ko.computed(function() {
+            return Math.round(gridData().length / pageSize());
+        });
+
+        var startRow = ko.computed(function() {
+            return (currentPage() * pageSize()) - pageSize();
+        });
+
+        var endRow = ko.computed(function() {
+            return (startRow() + pageSize()) - 1;
+        });
+
+        var wibble = function(obj) {
+            console.log(obj);
+        }
 
         if (!gridData().length) {
             return;
@@ -23,27 +34,37 @@ ko.components.register('simple-bootstrap-data-grid', {
                 cols.push(c);
             }
 
-            console.log(cols);
             return cols;
         }
     
 
         return {
             columnHeadings: columnHeadings,
-            gridData: gridData
+            gridData: gridData,
+            currentPage: currentPage,
+            maxPages: maxPages,
+            startRow: startRow,
+            endRow: endRow,
+            wibble: wibble
         }
     },
     template:
-        '<table class="table">\
+        '\
+        <p>Start row: <span data-bind="text: startRow"></span>. End row: <span data-bind="text: endRow"></span>\
+        <table class="table">\
             <thead>\
                 <tr data-bind="foreach: columnHeadings">\
-                    <th data-bind="text: $data"></th>\
+                    <th><a href="#" data-bind="text: $data"></a></th>\
                 </tr>\
             </thead>\
             <tbody data-bind="foreach: { data: gridData, as: \'gridData\' }">\
+                <!-- ko if: $index() >= $parent.startRow() && $index() <= $parent.endRow() -->\
                 <tr data-bind="foreach: { data: Object.keys(gridData), as: \'_propKey\' }">\
                     <td data-bind="text: gridData[_propKey]"></td>\
                 </tr>\
+                <!-- /ko -->\
             </tbody>\
-        </table>'
+        </table><br />\
+        <p>Page <span data-bind="text: currentPage"></span> of <span data-bind="text: maxPages"></span></p>\
+        '
 })
